5276ea017e8a087747b6c943777199abbf914b75
gabriel Michard <gabriel.michard@supelec.fr>
2022-10-09 10:33:43 +0200

    still testing

--- main.py
+++ main.py
@@ -637,7 +637,7 @@ def createTraining_post():
     if current_user.email != ADMIN_MAIL:
         return redirect(url_for('profile'))
     nameList=""
-    for name,value in request.form.to_dict():
+    for name in request.form:
         if name=="trainingName":
             trainingName=name
         else:
